9b5f4a1d64eb127a612254d4f69f9203e4164d2e
osantoschris <christianoliveira8@gmail.com>
2024-07-18 20:14:10 -0300

    correção no nome do tamplate

--- homepage/views.py
+++ homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'homepage/indez.html', {})
+    return render(request, 'homepage/index.html', {})
