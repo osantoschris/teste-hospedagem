217fa181ebd0b166b5f3e0def73da866a935d96a
osantoschris <christianoliveira8@gmail.com>
2024-07-18 20:06:11 -0300

    include vercel config

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'homepage.apps.HomepageConfig',
+    # 'homepage.apps.HomepageConfig',
+    'homepage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
