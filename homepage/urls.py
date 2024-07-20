755432123dd543dabc1929527dcd377d24fe5ebb
osantoschris <christianoliveira8@gmail.com>
2024-07-20 11:38:05 -0300

    personalização do menu dropdown

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('products-services/', views.products, name='products-services'),
 ]
