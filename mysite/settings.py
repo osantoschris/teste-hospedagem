a6a57dc897a52e535afc0a5e27bdec9c3f590c50
Christian Oliveira <52898213+osantoschris@users.noreply.github.com>
2024-07-22 17:07:22 -0300

    remove secret keys

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'christian.santos92@gmail.com'
-EMAIL_HOST_PASSWORD = 'cmmv qyfy ggwk rqtn'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 # EMAIL_HOST = 'smtp.gmail.com'
