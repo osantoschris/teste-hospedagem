cc4bd8e91392031dd61b32588ce5971e83a8a9fa
Christian Dos Santos Oliveira <christian.oliveira@hypera.com.br>
2024-07-22 16:57:25 -0300

    primeiros testes de envio de email

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+
+import smtplib
+import email.message
 
 # Create your views here.
 def index(request):
@@ -15,3 +21,29 @@ def contact(request):
 
 def clients(request):
     return render(request, 'homepage/clients.html', {})
+
+def send_email(request):
+    if request.method == 'POST':
+        nome = request.POST.get("nome")
+        mensagem = request.POST.get("msg")
+        destinatario = request.POST.get("email")
+
+        # Configuração do email
+        email_msg = email.message.EmailMessage()
+        email_msg['Subject'] = f'Email de {nome}'
+        email_msg['From'] = settings.EMAIL_HOST_USER
+        email_msg['To'] = destinatario
+        email_msg.set_content(mensagem, subtype='html')
+
+        # Conectar ao servidor SMTP
+        try:
+            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                smtp.starttls()
+                smtp.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+                smtp.send_message(email_msg)
+        except Exception as e:
+            return HttpResponse(f'Erro ao enviar email: {e}')
+
+        return HttpResponse('E-mail enviado com sucesso!')
+
+    return render(request, 'homepage/contact.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,16 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# # Send Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'christian.santos92@gmail.com'
+EMAIL_HOST_PASSWORD = 'cmmv qyfy ggwk rqtn'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'christian.santos92@gmail.com'
+# EMAIL_HOST_PASSWORD = 'oxty juiq sgoc jqhz'
