66daa3bd7288431047a0970b1362d972b7766f86
osantoschris <christianoliveira8@gmail.com>
2024-07-26 11:42:42 -0300

    commit para teste e envio de email em produção

--- homepage/static/homepage/js/script.js
+++ homepage/static/homepage/js/script.js
@@ -5,23 +5,23 @@ document.getElementById('menuIcon').addEventListener('click', function() {
 });
 
 
-function getValues() {
-    var name = document.getElementById('input_name').value.length;
-    var email = document.getElementById('input_email').value.length;
-    var message = document.getElementById('input_message').value.length;
+// function getValues() {
+//     var name = document.getElementById('input_name').value.length;
+//     var email = document.getElementById('input_email').value.length;
+//     var message = document.getElementById('input_message').value.length;
 
-    if (name < 1 && email < 1 && message < 1) {
-        console.log('Campos não preenchidos');
-    } else {
-        console.log('Campos preenchidos')  
-    };
-};
+//     if (name < 1 && email < 1 && message < 1) {
+//         console.log('Campos não preenchidos');
+//     } else {
+//         console.log('Campos preenchidos')  
+//     };
+// };
 
 
 
-function getName() {
-    var name = document.getElementById('input_name').value.length;
-    console.log(name);
-};
+// function getName() {
+//     var name = document.getElementById('input_name').value.length;
+//     console.log(name);
+// };
 
-document.getElementById('btnSubmit').addEventListener('click', getValues);
+// document.getElementById('btnSubmit').addEventListener('click', getValues);
--- staticfiles/homepage/js/script.js
+++ staticfiles/homepage/js/script.js
@@ -1,5 +1,27 @@
 document.getElementById('menuIcon').addEventListener('click', function() {
     this.classList.toggle('change');
-    document.getElementById('dropdownMenu').classList.toggle('show');
-    document.body.classList.toggle('no-scroll');
+    document.getElementById('dropdownMenu').classList.toggle('--show');
+    document.body.classList.toggle('--no-scroll');
 });
+
+
+// function getValues() {
+//     var name = document.getElementById('input_name').value.length;
+//     var email = document.getElementById('input_email').value.length;
+//     var message = document.getElementById('input_message').value.length;
+
+//     if (name < 1 && email < 1 && message < 1) {
+//         console.log('Campos não preenchidos');
+//     } else {
+//         console.log('Campos preenchidos')  
+//     };
+// };
+
+
+
+// function getName() {
+//     var name = document.getElementById('input_name').value.length;
+//     console.log(name);
+// };
+
+// document.getElementById('btnSubmit').addEventListener('click', getValues);
