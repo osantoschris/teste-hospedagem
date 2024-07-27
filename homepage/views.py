6d5ccf16ebad0386d7f58428c118b3fa7a24c045
osantoschris <christianoliveira8@gmail.com>
2024-07-27 18:31:26 -0300

    envio de email testado, subindo para produção

--- homepage/views.py
+++ homepage/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, HttpResponse
 from django.conf import settings
 from django.template.loader import get_template
 from django.core.mail import EmailMessage, send_mail
+from django.template.loader import render_to_string
 
 def send_contact_email(data):
     message_body = get_template('homepage/send.html').render(data)
@@ -23,18 +24,24 @@ def send_email_view(request):
         message = request.POST.get('message')
         from_email = settings.DEFAULT_FROM_EMAIL
         recipient_list = ['christianoliveira8@outlook.com']
-        data = {'name': name, 'email': email, 'message': message}
-        message_body = get_template('homepage/send.html').render(data)
-        print(message_body)
+        data = {
+            'name': name, 
+            'email': email, 
+            'message': message, 
+            'subject': subject
+        }
+        message_body = render_to_string('homepage/send.html', data)
 
-        print(type(settings.EMAIL_PORT))
-        print(type(settings.EMAIL_USE_TLS))
-        print(type(settings.EMAIL_TIMEOUT))
-        print(settings.EMAIL_PORT)
-        print(settings.EMAIL_USE_TLS)
-        print(settings.EMAIL_TIMEOUT)
         try:
-            send_mail(subject, message_body, from_email, recipient_list, fail_silently=False)
+            send_mail(
+                subject=subject,
+                message='', 
+                from_email=from_email, 
+                recipient_list=recipient_list, 
+                fail_silently=False,
+                html_message=message_body
+            )
+
             return render(request, 'homepage/contact.html')
         except Exception as e:
             return HttpResponse(f'Error: {e}', status=500)
@@ -56,22 +63,3 @@ def contact(request):
 
 def clients(request):
     return render(request, 'homepage/clients.html', {})
-
-# def send_email_view(request):
-
-#     if request.method == 'POST':
-        
-#         email = {
-#             'name': request.POST.get('name'),
-#             'email': request.POST.get('email'),
-#             'subject': f'Mensagem de {request.POST.get("name")}',
-#             'message': request.POST.get('message')
-#         }
-
-#         try:
-#             send_contact_email(email)
-#             return render(request, 'homepage/contact.html')
-#         except Exception as e:
-#             HttpResponse(f'Error: {e}', status=500)
-
-#     return render(request, 'homepage/contact.html')
